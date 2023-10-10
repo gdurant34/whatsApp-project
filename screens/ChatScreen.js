ab0cc1e626e134ad325d22b22a69d827c04b2630
Grace Durant <gdurant34@gmail.com>
2023-10-09 17:05:12 -0700

    updated chat screen with icons

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,26 +1,50 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, ImageBackground } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import backgroundImage from "../assets/images/droplet.jpeg";
+import { TextInput } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Feather } from "@expo/vector-icons";
+import colors from "../constants/colors";
 
-const ChatScreen = props => {
-
-    return  (
-        <View style={styles.container} >
-            <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
-
-            </ImageBackground>
-        </View>
-    )
+const ChatScreen = (props) => {
+  return (
+    <SafeAreaView edges={["right", "left", "bottom"]} style={styles.container}>
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.backgroundImage}
+      ></ImageBackground>
+      <View style={styles.inputContainer}>
+        <TouchableOpacity onPress={() => console.log("Pressed!")}>
+          <Feather name="plus" size={24} color={colors.blue} />
+        </TouchableOpacity>
+        <TextInput />
+        <TouchableOpacity onPress={() => console.log("Pressed!")}>
+          <Feather name="camera" size={24} color={colors.blue} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        flexDirection: 'column'
-    },
-    backgroundImage: {
-        flex: 1
-    }
-})
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  backgroundImage: {
+    flex: 1,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    height: 50,
+  },
+});
 
 export default ChatScreen;
