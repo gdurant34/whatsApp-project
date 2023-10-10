a9f3208be4e0d1b27b858a058e9321341fca037f
Grace Durant <gdurant34@gmail.com>
2023-10-09 23:43:07 -0700

    added logo and keyboard avoiding view

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import PageContainer from "../components/PageContainer";
 
-import SignUpForm from "../components/SignUpForm";
+import PageContainer from "../components/PageContainer";
 import SignInForm from "../components/SignInForm";
+import SignUpForm from "../components/SignUpForm";
 import colors from "../constants/colors";
+
+import logo from "../assets/images/logo.png";
 
 const AuthScreen = (props) => {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -13,31 +24,55 @@ const AuthScreen = (props) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <PageContainer>
-        {isSignUp ? <SignUpForm /> : <SignInForm />}
+        <ScrollView>
+          <KeyboardAvoidingView
+            style={styles.keyboardAvoidingView}
+            behavior={Platform.OS === "ios" ? "height" : undefined}
+            keyboardVerticalOffset={100}
+          >
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={logo} resizeMode="contain" />
+            </View>
 
-        <TouchableOpacity
-          onPress={() => setIsSignUp((prevState) => !prevState)}
-          style={styles.linkContainer}
-        >
-          <Text style={styles.link}>{`Switch to ${isSignUp ? "sign in" : "sign up"}`}</Text>
-        </TouchableOpacity>
+            {isSignUp ? <SignUpForm /> : <SignInForm />}
+
+            <TouchableOpacity
+              onPress={() => setIsSignUp((prevState) => !prevState)}
+              style={styles.linkContainer}
+            >
+              <Text style={styles.link}>{`Switch to ${
+                isSignUp ? "sign in" : "sign up"
+              }`}</Text>
+            </TouchableOpacity>
+          </KeyboardAvoidingView>
+        </ScrollView>
       </PageContainer>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-    linkContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginVertical: 15
-    },
-    link: {
-        color: colors.blue,
-        fontFamily: 'medium',
-        letterSpacing: 0.3
-    }
-
+  linkContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 15,
+  },
+  link: {
+    color: colors.blue,
+    fontFamily: "medium",
+    letterSpacing: 0.3,
+  },
+  imageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "50%",
+  },
+  keyboardAvoidingView: {
+    flex: 1,
+    justifyContent: "center",
+  },
 });
 
 export default AuthScreen;
